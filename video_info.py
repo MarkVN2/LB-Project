f83635cede58dc1c475f4b99552bfdf812285f90
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2025-05-13 08:12:31 -0300

    [feat]: transcript and livechat info

--- video_info.py
+++ video_info.py
@@ -1,4 +1,5 @@
 from youtool import YouTube
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound
 from pymongo import MongoClient
 import argparse
 import os
@@ -15,6 +16,10 @@ conn = MongoClient(MONGO_HOST, MONGO_PORT)
 API_KEY = os.getenv('YOUTUBE_API_KEY', '')
 yt = YouTube(API_KEY, disable_ipv6=True)
 
+def format_url(url: str) -> str:
+    formatted_url = url.split("v=")[-1].split("&")[0]
+    return formatted_url
+
 def get_video_data(video: str, data_collection_level: int, show: bool) -> None:
     """
     Fetch and process video data from YouTube API.
@@ -28,23 +33,42 @@ def get_video_data(video: str, data_collection_level: int, show: bool) -> None:
     print(f"[INFO] Data collection level: {data_collection_level}")
     print(f"[INFO] Show output: {show}")
 
+    video = format_url(video)
+
     try:
         main_info = next(yt.videos_infos([video]))
     except StopIteration:
         print(f"[ERROR] No video info found for ID: {video}")
         return
-    if data_collection_level >= 2: 
-        comments_info = next(yt.video_comments(video))
-
+    
     video_data = {
         'title':main_info['title'],
         'description':main_info['description'],
-        'comments':comments_info,
         'viewCount':main_info['views'],
-        'likeCount':main_info['likes'],
+        'likeCount':main_info.get('likes',0),
         'dislikeCount':main_info.get('dislikes', 0),   
     }
     
+    if data_collection_level >= 2: 
+        try:
+            comments_info = next(yt.video_comments(video))
+            video_data['comments'] = comments_info
+        except: 
+            print("[WARNING] No Comments")
+    if data_collection_level >= 3:
+        try:
+            transcript = YouTubeTranscriptApi.get_transcript(video, languages=['en', 'pt'])
+            video_data['transcript'] = transcript
+        except (TranscriptsDisabled, NoTranscriptFound):
+            print("[WARNING] Transcript not available.")
+        except Exception as e:
+            print(f"[ERROR] Failed to fetch transcript: {e}")
+    if data_collection_level >= 4:
+        try:
+            livechat_info = next(yt.video_livechat(video))
+            video_data['livechat'] = livechat_info
+        except:
+            print("[WARNING] No Live Chat Replay")
     if show:
         from pprint import pprint
         pprint(video_data)  # Replace with real output
